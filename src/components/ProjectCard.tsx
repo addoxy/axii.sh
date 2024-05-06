37d9672d8a7475a5ddbc1570d52bb05d58ec1739
addoxy <adityakumar13043@gmail.com>
2024-05-06 17:18:39 +0530

    feat:  make projects section responsive

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -14,17 +14,34 @@ const ProjectCard = (props: ProjectCardProps) => {
   const { title, description, imagePath, href } = props;
 
   return (
-    <div className="bg-card flex w-full gap-10 rounded-3xl border border-border px-14 py-12">
+    <div className="bg-card flex w-full flex-col gap-10 rounded-xl border border-border px-8 py-6 sm:rounded-2xl sm:px-10 sm:py-8 md:rounded-3xl md:px-14 md:py-12 lg:flex-row">
       <div className="flex flex-col justify-between">
         <div className="flex flex-col">
-          <h3 className="text-3xl font-semibold text-primary">{title}</h3>
+          <div className="flex items-center justify-between">
+            <h3 className="text-2xl font-semibold text-primary sm:text-3xl">
+              {title}
+            </h3>
+            <Button
+              asChild
+              className="h-12 w-fit rounded-md bg-primary px-4 text-lg font-bold text-background sm:text-xl lg:hidden"
+            >
+              <a
+                href={href}
+                target="_blank"
+                className="flex items-center gap-3"
+              >
+                Visit Project
+                <ArrowRight className="size-5 sm:size-6" />
+              </a>
+            </Button>
+          </div>
           <p className="mt-11 text-lg font-medium text-foreground">
             {description}
           </p>
         </div>
         <Button
           asChild
-          className="h-12 w-fit rounded-md bg-primary px-4 text-xl font-bold text-background"
+          className="hidden h-12 w-fit rounded-md bg-primary px-4 text-xl font-bold text-background lg:flex"
         >
           <a href={href} target="_blank" className="flex items-center gap-3">
             Visit Project
--- src/components/ui/tablet.tsx
+++ src/components/ui/tablet.tsx
@@ -8,7 +8,7 @@ const Tablet = (props: LaptopProps) => {
   const { imagePath } = props;
 
   return (
-    <div className="bg-tablet max-w-[512px] shrink-0 rounded-xl p-3 shadow-lg shadow-background">
+    <div className="bg-tablet shrink-0 rounded-xl p-3 shadow-lg shadow-background lg:max-w-[400px] xl:max-w-[512px]">
       <Image
         src={imagePath}
         className="relative w-full rounded-lg"
