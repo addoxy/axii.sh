9d3fd77092ac005eb0b56d2803137a558d0dafb8
addoxy <adityakumar13043@gmail.com>
2024-05-06 17:47:54 +0530

    feat:  improve the responsiveness of project cards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,31 +17,16 @@ const ProjectCard = (props: ProjectCardProps) => {
     <div className="bg-card flex w-full flex-col gap-10 rounded-xl border border-border px-8 py-6 sm:rounded-2xl sm:px-10 sm:py-8 md:rounded-3xl md:px-14 md:py-12 lg:flex-row">
       <div className="flex flex-col justify-between">
         <div className="flex flex-col">
-          <div className="flex items-center justify-between">
-            <h3 className="text-2xl font-semibold text-primary sm:text-3xl">
-              {title}
-            </h3>
-            <Button
-              asChild
-              className="h-12 w-fit rounded-md bg-primary px-4 text-lg font-bold text-background sm:text-xl lg:hidden"
-            >
-              <a
-                href={href}
-                target="_blank"
-                className="flex items-center gap-3"
-              >
-                Visit Project
-                <ArrowRight className="size-5 sm:size-6" />
-              </a>
-            </Button>
-          </div>
-          <p className="mt-11 text-lg font-medium text-foreground">
+          <h3 className="text-2xl font-semibold text-primary sm:text-3xl">
+            {title}
+          </h3>
+          <p className="mt-6 text-lg font-medium text-foreground sm:mt-11">
             {description}
           </p>
         </div>
         <Button
           asChild
-          className="hidden h-12 w-fit rounded-md bg-primary px-4 text-xl font-bold text-background lg:flex"
+          className="mt-6 h-12 w-fit rounded-md bg-primary px-4 text-xl font-bold text-background lg:mt-0"
         >
           <a href={href} target="_blank" className="flex items-center gap-3">
             Visit Project
